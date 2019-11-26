d5303e9da0c002c193f2f533468e4641122dc9ee
David Rigan <david.rigan@students.amikom.ac.id>
2019-11-26 21:31:06 +0700

    membuat input output pada python

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print('hello world!')
+"""
+a = 5
+nama = 'david'
+print('nilai variabel a adalah ', a)
+print('nama saya adalah %s ' % nama)
+print('nama saya %s dengan umur %d tahun' % (nama, a))
+"""
+num = input('masukan umur kamu: ')
+print('umur saya %s tahun' % num)
