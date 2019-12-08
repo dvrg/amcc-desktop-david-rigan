39f66663bc082f3857a605351628b1231e572321
David Rigan <david.rigan@students.amikom.ac.id>
2019-12-08 20:36:15 +0700

    #10 variabel

--- basic.py
+++ basic.py
@@ -1,2 +1,10 @@
 import datetime
 print('waktu sekarang adalah :', datetime.datetime.now())
+
+"""Variabel"""
+mynow = datetime.datetime.now()
+print(mynow)
+
+number = 10
+string = 'David Rigan'
+print(number, string)
