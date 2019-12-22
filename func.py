81be933b645b6aa2c3a01ad9d8d39d905ed1ba33
David Rigan <davidrigan.dev@gmail.com>
2019-12-22 23:47:30 +0700

    update function

--- func.py
+++ func.py
@@ -1,4 +1,10 @@
 def square(x):
     """Calculate Square Value"""
     print("luas persegi dengan x =", x)
-    return x*x    
+    return x*x
+
+def password(x):
+    if len(x) >= 8:
+        print('password berhasil dibuat. Jumlah karakter', len(x))
+    else:
+        print('password gagal dibuat. Jumlah karakter', len(x))
