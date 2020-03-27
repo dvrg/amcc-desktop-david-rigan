6244a5f87efcafb89b0ef15963c950a842a2d3df
David Rigan <davidrigan.dev@gmail.com>
2020-03-27 17:23:29 +0700

    #1 advance in function, #2 using argument, #3 calculation argument values, #4 loop argument values

--- func.py
+++ func.py
@@ -8,3 +8,35 @@ def password(x):
         print('password berhasil dibuat. Jumlah karakter', len(x))
     else:
         print('password gagal dibuat. Jumlah karakter', len(x))
+
+def area(a, b=8):
+    return a*b
+
+#print(area(a=7)) # Parameter dengan argument
+
+def mean(*args):
+    return sum(args)
+
+#def mean(**kwargs):
+
+#print(mean(1,2,3))
+
+#print(mean(x=1,y=2,z=3))
+
+# buat sebuah fungsi, foo(10, 20, 30 40) dimana, nilai yang dikirimkan ini harus mengembalikan nilai 25,
+# yaitu rata2 dari nilai yang dikirim
+
+
+def foo(*args):
+    return sum(args) / len(args)
+
+#print(foo(10,20,30,40)) 
+
+# buat sebuah fungsi dimana jika fungsi func("api", "air", "udara", "tanah") bakal mengembalikan ["API", "AIR", "UDARA", "TANAH"]
+# buat urutan datanya ascending (A-Z) jadi ["AIR", "API", "TANAH", "UDARA"]
+
+def func(*args):
+    args = [x.upper() for x in args]
+    return sorted(args)
+
+print(func("api", "air", "udara", "tanah"))
